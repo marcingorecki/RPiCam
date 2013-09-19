0f8d396854b657c90feccecd8bcc57ef91e43025
Marcin Gorecki <mgorecki@post.pl>
2013-09-19 23:25:15 +0000

    Client now obeys QUIT command

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import serial
 import time
 import photo
+import common
 
 port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
 
@@ -8,6 +9,8 @@ while True:
     waiting = port.inWaiting()
     if(waiting > 0): 
 	data = port.read(waiting)
+        if data == "QUIT":
+	   common.shutdown()	
         photo.take_photo(data)
         print "Saved photo "+data
     time.sleep(0.1)
